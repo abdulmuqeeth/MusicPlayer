96851667f1863ec5e41f46634a69825cf4948307
abdulmuqeethmohammed <amoham62@uic.edu>
2018-02-22 15:18:28 -0600

    Creating a Songs Class with necessary fields

---
+++ app/src/main/java/com/abdulmuqeethmohammed/musicplayer/Songs.java
@@ -0,0 +1,37 @@
+package com.abdulmuqeethmohammed.musicplayer;
+
+/**
+ * Created by Abdul Muqeeth Mohammed
+ */
+
+public class Songs {
+    private String songTitle;
+    private String artistName;
+    private String songWiki;
+    private String videoUrl;
+
+    public String getSongTitle() {
+        return songTitle;
+    }
+    public void setSongTitle(String songTitle) {
+        this.songTitle = songTitle;
+    }
+    public String getArtistName() {
+        return artistName;
+    }
+    public void setArtistName(String artistName) {
+        this.artistName = artistName;
+    }
+    public String getSongWiki() {
+        return songWiki;
+    }
+    public void setSongWiki(String songWiki) {
+        this.songWiki = songWiki;
+    }
+    public String getVideoUrl() {
+        return videoUrl;
+    }
+    public void setVideoUrl(String videoUrl) {
+        this.videoUrl = videoUrl;
+    }
+}
