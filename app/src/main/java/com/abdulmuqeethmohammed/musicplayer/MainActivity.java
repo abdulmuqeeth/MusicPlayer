1f2f3c6558db53bc3b2087ac756cc1e643b594f3
abdulmuqeethmohammed <amoham62@uic.edu>
2018-02-24 22:09:35 -0600

    Linking the custom adapter to the main page list and adding 5 default songs to the list

--- app/src/main/java/com/abdulmuqeethmohammed/musicplayer/MainActivity.java
+++ app/src/main/java/com/abdulmuqeethmohammed/musicplayer/MainActivity.java
@@ -4,13 +4,32 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<Songs> songsArrayList;
+    private ListView mainList;
+    private Songs song;
+    private SongAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        songsArrayList = new ArrayList<Songs>();
+        songsArrayList.add(new Songs("Payphone","Maroon 5", "https://en.wikipedia.org/wiki/Payphone_(song)","https://www.youtube.com/watch?v=KRaWnd3LJfs"));
+        songsArrayList.add(new Songs("Bailamos","Enrique Iglesias", "https://en.wikipedia.org/wiki/Bailamos","https://www.youtube.com/watch?v=ixqOgtNERAQ"));
+        songsArrayList.add(new Songs("Hips Don't Lie","Shakira", "https://en.wikipedia.org/wiki/Hips_Don%27t_Lie","https://www.youtube.com/watch?v=DUT5rEU6pqM"));
+        songsArrayList.add(new Songs("Hey There Delilah","Plain White T's", "https://en.wikipedia.org/wiki/Hey_There_Delilah","https://www.youtube.com/watch?v=h_m-BjrxmgI"));
+        songsArrayList.add(new Songs("If We Ever Meet Again","Timbaland", "https://en.wikipedia.org/wiki/If_We_Ever_Meet_Again","https://www.youtube.com/watch?v=KDKva-s_khY"));
+
+        adapter = new SongAdapter(this, R.layout.listview_item, songsArrayList);
+        mainList = (ListView) findViewById(R.id.main_list);
+        mainList.setAdapter(adapter);
     }
 
     @Override
--- app/src/main/java/com/abdulmuqeethmohammed/musicplayer/Songs.java
+++ app/src/main/java/com/abdulmuqeethmohammed/musicplayer/Songs.java
@@ -10,28 +10,23 @@ public class Songs {
     private String songWiki;
     private String videoUrl;
 
+    public Songs(String songTitle, String artistName, String songWiki, String videoUrl) {
+        this.songTitle = songTitle;
+        this.artistName = artistName;
+        this.songWiki = songWiki;
+        this.videoUrl = videoUrl;
+    }
+
     public String getSongTitle() {
         return songTitle;
-    }
-    public void setSongTitle(String songTitle) {
-        this.songTitle = songTitle;
     }
     public String getArtistName() {
         return artistName;
     }
-    public void setArtistName(String artistName) {
-        this.artistName = artistName;
-    }
     public String getSongWiki() {
         return songWiki;
     }
-    public void setSongWiki(String songWiki) {
-        this.songWiki = songWiki;
-    }
     public String getVideoUrl() {
         return videoUrl;
-    }
-    public void setVideoUrl(String videoUrl) {
-        this.videoUrl = videoUrl;
     }
 }
