42eff862c975c58eb652712319dae721534fc836
abdulmuqeethmohammed <amoham62@uic.edu>
2018-02-27 01:10:01 -0600

    Adding Artist Wiki field to the Songs model class and updating Add Song dialog box layout to include it

--- app/src/main/java/com/abdulmuqeethmohammed/musicplayer/Songs.java
+++ app/src/main/java/com/abdulmuqeethmohammed/musicplayer/Songs.java
@@ -9,12 +9,14 @@ public class Songs {
     private String artistName;
     private String songWiki;
     private String videoUrl;
+    private String artistWiki;
 
-    public Songs(String songTitle, String artistName, String songWiki, String videoUrl) {
+    public Songs(String songTitle, String artistName, String songWiki, String videoUrl, String artistWiki) {
         this.songTitle = songTitle;
         this.artistName = artistName;
         this.songWiki = songWiki;
         this.videoUrl = videoUrl;
+        this.artistWiki = artistWiki;
     }
 
     public String getSongTitle() {
@@ -28,5 +30,8 @@ public class Songs {
     }
     public String getVideoUrl() {
         return videoUrl;
+    }
+    public String getArtistWiki() {
+        return artistWiki;
     }
 }
