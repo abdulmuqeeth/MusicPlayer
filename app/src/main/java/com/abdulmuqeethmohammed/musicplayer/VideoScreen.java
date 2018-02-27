17049fb401cc0a8c22373bb3c818f017761f4bff
abdulmuqeethmohammed <amoham62@uic.edu>
2018-02-27 00:52:46 -0600

    Creating VideoScreen Activity and integrating a WebView into it

---
+++ app/src/main/java/com/abdulmuqeethmohammed/musicplayer/VideoScreen.java
@@ -0,0 +1,15 @@
+package com.abdulmuqeethmohammed.musicplayer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VideoScreen extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_screen);
+
+    }
+}
